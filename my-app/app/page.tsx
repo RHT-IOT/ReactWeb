5877f9cbd35fe408e293783c58c156ae5cd08417
AmberTang <72559487+TangYanYee@users.noreply.github.com>
2025-10-02 00:42:18 +0800

    get user name

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,34 @@
 // App.js
 "use client";
-
+import { useEffect, useState } from "react";
 import { useAuth } from "react-oidc-context";
 
 function App() {
   const auth = useAuth();
+  const [userInfo, setUserInfo] = useState({ username: "" , name: ""});
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+
+      console.log("Hi");
+      if (auth.isAuthenticated) {
+        const response = await fetch(
+          "https://ap-southeast-2d19wijvbp.auth.ap-southeast-2.amazoncognito.com/oauth2/userInfo",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${auth.user.access_token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        console.log("User Info:", data);
+        setUserInfo(data);
+      }
+    };
+
+    fetchUserInfo();
+  }, [auth.isAuthenticated, auth.user?.access_token]);
 
   const signOutRedirect = () => {
     const clientId = "7bj6qolgca3bbcshiuiinp9tj4";
@@ -20,12 +44,11 @@ function App() {
   if (auth.error) {
     return <div>Encountering error... {auth.error.message}</div>;
   }
-
   if (auth.isAuthenticated) {
     return (
       <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> Username {auth.user?.profile.phone} </pre>
+        {/* <pre> Hello: {auth.user?.profile.email} </pre> */}
+        <pre> Welcome {userInfo.username}!!</pre>
         <pre> ID Token: {auth.user?.id_token} </pre>
         <pre> Access Token: {auth.user?.access_token} </pre>
         <pre> Refresh Token: {auth.user?.refresh_token} </pre>
